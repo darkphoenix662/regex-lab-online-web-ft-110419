d8ac1f0fdc34cea3468a966591d50fd0178bf666
Sean Good <tree662@protonmail.com>
2019-11-16 02:29:00 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,8 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.scan(/(\d+)-(\d+)-(\d+)/)
+  if phone.scan(/(\d+)-(\d+)-(\d+)/)
+  else
+  false
+end
 end
