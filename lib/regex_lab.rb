665ce47a91de2b70a56ea373826c029c2e5920c4
Sean Good <tree662@protonmail.com>
2019-11-16 00:37:09 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,8 +3,8 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text = 
-  text =
+  text = ~/^un/
+  text = ~/ing$/
 end
 
 def words_five_letters_long(text)
